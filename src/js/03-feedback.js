803e1e08f674ffd31eda6435ce5552b76c1dbbf0
KarolinaBrzezinska <91696944+KarolinaBrzezinska@users.noreply.github.com>
2022-03-27 21:05:17 +0200

    x

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('form.feedback-form');
+const emailField = document.querySelector('input');
+const messageField = document.querySelector('textarea');
+
+const inputCatcher = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = form;
+    
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: email.value,
+      message: message.value,
+    }),
+  );
+};
+
+form.addEventListener('input', throttle(inputCatcher, 500));
+
+const inputVal = localStorage.getItem('feedback-form-state');
+const parsedInputVal = JSON.parse(inputVal);
+
+console.log(parsedInputVal);
+
+
+if (parsedInputVal) {
+  if (parsedInputVal && parsedInputVal.email !== '') {
+    emailField.value = parsedInputVal.email;
+  }
+}  
+
+if (parsedInputVal) {
+  if (parsedInputVal && parsedInputVal.message !== '') {
+    messageField.value = parsedInputVal.message;
+  }
+}  
+
+
+
+const submitHandler = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = form;
+  
+  console.log(`Email: ${email.value}, Message: ${message.value}`);
+  
+  form.reset();
+  
+  localStorage.removeItem('feedback-form-state');
+}
+
+form.addEventListener('submit', submitHandler);
