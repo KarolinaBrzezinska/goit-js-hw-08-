daf6b335bad431332b4976a3f9511d7a0030daba
KarolinaBrzezinska <91696944+KarolinaBrzezinska@users.noreply.github.com>
2022-03-28 19:15:43 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const $div = document.querySelector(".gallery");
+
+const smallImage = [];
+const bigImage = [];
+const altImage = [];
+const HTMLToWebsite = [];
+
+for (const item of galleryItems) {
+  smallImage.push(item.preview);
+  bigImage.push(item.original);
+  altImage.push(item.description);
+}
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const htmlWithNewVariables = `<a class="gallery__item" href="${bigImage[i]}"><img class="gallery__image" src="${smallImage[i]}" alt="${altImage[i]}"</a>`;
+  HTMLToWebsite.push(htmlWithNewVariables);
+}
+const readyHTMLToWebsite = HTMLToWebsite.join("");
+
+$div.insertAdjacentHTML("afterbegin", readyHTMLToWebsite);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,  
+});
+
