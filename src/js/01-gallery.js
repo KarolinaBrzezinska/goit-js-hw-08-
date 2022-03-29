3ca12b1ff1131c0fb91771db96900f1aacf0fd83
KarolinaBrzezinska <91696944+KarolinaBrzezinska@users.noreply.github.com>
2022-03-29 21:00:26 +0200

    x

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,33 +1,23 @@
 // Add imports above this line
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const $div = document.querySelector(".gallery");
-
-const smallImage = [];
-const bigImage = [];
-const altImage = [];
+const $div = document.querySelector('.gallery');
 const HTMLToWebsite = [];
 
 for (const item of galleryItems) {
-  smallImage.push(item.preview);
-  bigImage.push(item.original);
-  altImage.push(item.description);
-}
-
-for (let i = 0; i < galleryItems.length; i++) {
-  const htmlWithNewVariables = `<a class="gallery__item" href="${bigImage[i]}"><img class="gallery__image" src="${smallImage[i]}" alt="${altImage[i]}"</a>`;
+  const htmlWithNewVariables = `<a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"</a>`;
   HTMLToWebsite.push(htmlWithNewVariables);
 }
-const readyHTMLToWebsite = HTMLToWebsite.join("");
 
-$div.insertAdjacentHTML("afterbegin", readyHTMLToWebsite);
+const readyHTMLToWebsite = HTMLToWebsite.join('');
 
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionSelector: "img",
-  captionsData: "alt",
-  captionDelay: 250,  
+$div.insertAdjacentHTML('afterbegin', readyHTMLToWebsite);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
 });
-
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,21 @@
-
 import throttle from 'lodash.throttle';
+
+const emailFromUser = document.querySelector('input');
+
+emailFromUser.addEventListener('submit');
+
+console.log(emailAndMessageFromUser);
 
 const form = document.querySelector('form.feedback-form');
 const emailField = document.querySelector('input');
 const messageField = document.querySelector('textarea');
 
-const inputCatcher = (event) => {
+const inputCatcher = event => {
   event.preventDefault();
   const {
     elements: { email, message },
   } = form;
-    
+
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify({
@@ -27,33 +32,30 @@ const parsedInputVal = JSON.parse(inputVal);
 
 console.log(parsedInputVal);
 
-
 if (parsedInputVal) {
   if (parsedInputVal && parsedInputVal.email !== '') {
     emailField.value = parsedInputVal.email;
   }
-}  
+}
 
 if (parsedInputVal) {
   if (parsedInputVal && parsedInputVal.message !== '') {
     messageField.value = parsedInputVal.message;
   }
-}  
+}
 
-
-
-const submitHandler = (event) => {
+const submitHandler = event => {
   event.preventDefault();
 
   const {
     elements: { email, message },
   } = form;
-  
+
   console.log(`Email: ${email.value}, Message: ${message.value}`);
-  
+
   form.reset();
-  
+
   localStorage.removeItem('feedback-form-state');
-}
+};
 
 form.addEventListener('submit', submitHandler);
